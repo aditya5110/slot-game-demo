f3cfc04ad1a45424914dcd33a38b214933963af1
Aditya <sharmaditya5150@gmail.com>
2023-12-27 17:14:32 +0530

    optional minor change -  works eitherway

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Slots from './Slots.jsx'
+import Slots from './Slots'
 import './App.css'
 
 function App() {
